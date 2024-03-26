5924b956e242fe1ed1fb12627f5af49a42ede399
vigimus <160164175+vigimus@users.noreply.github.com>
2024-03-26 19:12:43 +0100

    Add files via upload

---
+++ javaScript_Kalkylator.js
@@ -0,0 +1,10 @@
+
+// Skapa en kalkylator! med hjälp av prompt(), och variabler, skapa ett program som adderar fyra tal:
+
+var forstaNumret = prompt("Ge mig det första talet du vill att jag ska addera");
+var andraNumret = prompt("Ge mig det andra talet du vill att jag ska addera");
+var tredjeNumret = prompt("Ge mig det tredje talet du vill att jag ska addera");
+var fjardeNumret = prompt("Ge mig det fjärde talet du vill att jag ska addera");
+
+alert(Number(forstaNumret) + Number(andraNumret) + Number(tredjeNumret) + Number(fjardeNumret));
+
